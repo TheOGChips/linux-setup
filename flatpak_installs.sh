de77b4565d41ec232af440b1e12c86e7dd5ce303
TheOGChips <swindell.christian.g@gmail.com>
2022-08-18 20:49:41 -0500

    Filesystem access overidden for Microsoft Teams.

--- flatpak_installs.sh
+++ flatpak_installs.sh
@@ -4,4 +4,4 @@ sudo dnf -y install flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install discord onlyoffice rpi-imager teams #zoom slack stm32cubeide
 sudo flatpak override com.discordapp.Discord --filesystem=home
-#sudo flatpak override com.microsoft.Teams --filesystem=home
+sudo flatpak override com.microsoft.Teams --filesystem=home
