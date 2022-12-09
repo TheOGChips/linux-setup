de124e9f76b72e85b4f4af675540e01f0bae4012
TheOGChips <swindell.christian.g@gmail.com>
2022-12-09 03:15:17 -0600

    Fixed some bugs and converted to Ansible up to Nano config; Skipped to add Git config

--- nano_config.sh
+++ nano_config.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # A script to configure my setup of the Nano text editor
 
-NANORC=~/.nanorc
-echo 'set autoindent' >> "$NANORC"
-echo 'set linenumbers' >> "$NANORC"
-echo 'set smarthome' >> "$NANORC"
-echo 'set tabsize 4' >> "$NANORC"
-echo 'set tabstospaces' >> "$NANORC"
-echo 'set numbercolor brightcyan' >> "$NANORC"
-echo 'set titlecolor brightyellow' >> "$NANORC"
-echo 'set functioncolor blue' >> "$NANORC"	# Will probably want green if using through SSH
-echo 'set keycolor brightwhite' >> "$NANORC"
+NANORC="$HOME"/.nanorc
+echo > "$NANORC"
+
+#NOTE: Will probably want 'functioncolor green' if using through SSH
+echo -n > "$NANORC"
+for setting in autoindent linenumbers smarthome 'tabsize 4' tabstospaces 'numbercolor brightcyan' 'titlecolor brightyellow' 'functioncolor blue' 'keycolor brightwhite'
+    do
+    echo "set $setting" >> "$NANORC"
+done
 echo 'include "/usr/share/nano/*.nanorc"' >> "$NANORC"
--- visudo_edits.sh
+++ visudo_edits.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 pkg_mgr="$1"
 alt_pkg_mgr="$2"
+ALIASES="$3"
 
 echo "\nThe following lines must be added from visudo AT THE END of the file:"
 for cmd in "$pkg_mgr" "$alt_pkg_mgr" arp poweroff reboot
     do
-    echo -e "\t$USER $HOSTNAME = (root) NOPASSWD: $(which cmd)"
+    echo -e "\t$USER $HOSTNAME = (root) NOPASSWD: $(sudo which $cmd)"
     echo "alias "$cmd"='sudo $cmd'" >> "$ALIASES"
 done
 echo -e "\nA terminal window under visudo will now launch. You will be taken to the editor after entering"
 echo "your password. The terminal window will close automatically after you exit visudo."
-sleep 5
-konsole -e 'sudo visudo'
+sleep 1 # TODO: Return this to 5 after debugging is done
+#konsole -e 'sudo visudo'
