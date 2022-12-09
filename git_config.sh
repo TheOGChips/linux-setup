5384d31945eb66e35799ae7627f88e16cd002393
TheOGChips <swindell.christian.g@gmail.com>
2022-12-09 03:43:29 -0600

    Git config shell script no longer needed

--- git_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pkg_mgr="$1"
-sudo "$pkg_mgr" -y install git
-
-git config --global user.name "TheOGChips"
-git config --global user.email "swindell.christian.g@gmail.com"
-git config --global credential.helper store
-git config --global core.sshCommand "ssh -i ~/.ssh/github -F /dev/null"
