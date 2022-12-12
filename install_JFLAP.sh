68577b9d9d4fdfa89f41f0481f20ff70e15e8ecc
TheOGChips <swindell.christian.g@gmail.com>
2022-12-11 21:08:13 -0600

    Removed JFLAP install shell script

--- install_JFLAP.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-JFLAP=JFLAP7.1.jar
-wget "https://www.jflap.org/jflaptmp/july27-18/$JFLAP" #change URL as necessary
-mv "$JFLAP" "$HOME"/.local/bin
-echo "alias jflap='java -jar $HOME/.local/bin/$JFLAP'" >> "$HOME"/.bash_aliases
