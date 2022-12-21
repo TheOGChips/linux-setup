b5157cd40a678db3f05e1751cfa75b8a2436bf2d
TheOGChips <swindell.christian.g@gmail.com>
2022-12-20 22:27:23 -0600

    Final fixes before first working production version

--- visudo_edits.sh
+++ visudo_edits.sh
@@ -3,11 +3,11 @@ pkg_mgr="$1"
 alt_pkg_mgr="$2"
 
 echo "\nThe following lines must be added from visudo AT THE END of the file:"
-for cmd in "$pkg_mgr" "$alt_pkg_mgr" arp poweroff reboot
+for cmd in "$pkg_mgr" "$alt_pkg_mgr" arp poweroff reboot ansible-playbook
     do
     echo -e "\t$USER $HOSTNAME = (root) NOPASSWD: $(sudo which $cmd)"
 done
 echo -e "\nA terminal window under visudo will now launch. You will be taken to the editor after entering"
 echo "your password. The terminal window will close automatically after you exit visudo."
-sleep 1 # TODO: Return this to 5 after debugging is done
-#konsole -e 'sudo visudo'
+sleep 5
+konsole -e 'sudo visudo'
